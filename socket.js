16ebb020c9913e9c31bf2f83055999e0d349f378
Ivan <dangelzm@gmail.com>
2015-09-24 14:24:14 +0300

    Fix request without results

--- socket.js
+++ socket.js
@@ -1,44 +1,4 @@
 var request = require('request');
-var token = "081D75D074AA4092BD6F7720637B0969";
-
-/*var jsonForTest = [
-  {
-    "Outcome": "Success",
-    "Message": null,
-    "Identity": "Request",
-    "Delay": 0.0067632,
-    "BaseCurrency": "EUR",
-    "QuoteCurrency": "USD",
-    "Symbol": "EURUSD",
-    "Date": "09/18/2015",
-    "Time": "8:59:59 PM",
-    "QuoteType": "Spot",
-    "Bid": 1.1298,
-    "Mid": 1.1303,
-    "Ask": 1.1308,
-    "Spread": 0.001,
-    "Text": "1 European Union euro = 1.1303 United States dollars",
-    "Source": "SIX Financial Information, Buyer = \"ICAP PLC London, Premium Spot Forex\", Seller = \"ICAP PLC London, Premium Spot Forex\""
-  },
-  {
-    "Outcome": "Success",
-    "Message": null,
-    "Identity": null,
-    "Delay": 0,
-    "BaseCurrency": "AUD",
-    "QuoteCurrency": "USD",
-    "Symbol": "AUDUSD",
-    "Date": "09/18/2015",
-    "Time": "8:59:59 PM",
-    "QuoteType": "Spot",
-    "Bid": 0.71673,
-    "Mid": 0.71888,
-    "Ask": 0.72103,
-    "Spread": 0.0043,
-    "Text": "1 Australian dollar = 0.71888 United States dollar",
-    "Source": "SIX Financial Information, Buyer = \"ICAP PLC London, Premium Spot Forex\", Seller = \"ICAP PLC London, Premium Spot Forex\""
-  }
-];*/
 
 function getData(io) {
   var currencies = [
@@ -54,15 +14,20 @@ function getData(io) {
     '"USDTRY"'
   ].join(',');
 
-
-
-  var apiUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.xchange where pair in (' + currencies + ')&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys';
+  var apiUrl = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.xchange where pair in (' + currencies + ')&format=json&env=store://datatables.org/alltableswithkeys';
 
   request({
     url: apiUrl
   }, function(error, response, body) {
-    //console.log(JSON.parse(body).query.results.rate);
-    io.sockets.emit('message', JSON.parse(body).query.results.rate);
+    console.log(JSON.parse(body));
+    var resultBody = JSON.parse(body);
+
+    if(resultBody.query && resultBody.query.results){
+      io.sockets.emit('message', resultBody.query.results.rate);
+    } else {
+      console.log('No results');
+    }
+
     setTimeout(function () {
       getData(io);
     }, 300);
